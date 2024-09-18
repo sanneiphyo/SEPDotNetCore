7e474412f250bfca0d34d7118556ca7572444415
sanneiphyo <https://github.com/sanneiphyo>
2024-09-18 21:58:40 +0630

    Feat : ADOdotNet Crud

--- SEPDotNetCore.ConsoleApp/Program.cs
+++ SEPDotNetCore.ConsoleApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using SEPDotNetCore.ConsoleApp;
 using System.Data;
 using System.Data.SqlClient;
 
@@ -22,41 +23,7 @@ Console.WriteLine("Hello, World!");
 //F10     => 50  [to skip another line]
 //F5
 
-string connectionString = "Data Source =.;Initial Catalog=SEPDotNetCore;User ID =sa;Password=sasa@123";
-Console.WriteLine("Connection string :" + connectionString);
-SqlConnection connection = new SqlConnection(connectionString );
-
-
-
-Console.WriteLine("Connection Opening ...");
-connection.Open();
-Console.WriteLine("Connection Opened.");
-
-string query = @"SELECT [BlogId]
-,[BlogTitle]
-,[BlogAuthor]
-,[BlogContent]
-,[DeleteFlag]
-FROM [dbo].[Tbl_Blog]";
-
-SqlCommand cmd = new SqlCommand(query , connection);
-//SqlDataAdapter adapter = new SqlDataAdapter(cmd); //generator to run cmd
-//DataTable dt = new DataTable();
-//adapter.Fill(dt);
-
-SqlDataReader reader = cmd.ExecuteReader();
-while (reader.Read())
-{
-    Console.WriteLine(reader["BlogId"]);
-    Console.WriteLine(reader["BlogTitle"]);
-    Console.WriteLine(reader["BlogAuthor"]);
-    Console.WriteLine(reader["BlogContent"]);
-    Console.WriteLine(reader["DeleteFlag"]);
-};
-
-Console.WriteLine("Connection closing ...");
-connection.Close();
-Console.WriteLine("Connection closed...");
+AdoDotNet adoDotNet = new AdoDotNet();
 
 //Dataset = a collection of data (in table)
 //DataTable => dt
