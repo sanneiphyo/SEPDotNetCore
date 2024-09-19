12e5eb1edde9551aa17e79ef4125e70da9148d1b
sanneiphyo <https://github.com/sanneiphyo>
2024-09-19 16:42:48 +0630

    feat :add  BlogModel

---
+++ SEPDotNetCore.ConsoleApp/Models/BlogDataModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEPDotNetCore.ConsoleApp.Models
+{
+    internal class BlogDataModel
+    {
+        public string BlogId { get; set; }
+        public string BlogTitle { get; set; }
+        public string BlogAuthor { get; set; }
+        public string BlogContent{ get; set; }
+
+    }
+}
