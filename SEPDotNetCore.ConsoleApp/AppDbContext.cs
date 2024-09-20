9db5741786e082a2e70f78b146c46f48fb56415b
sanneiphyo <https://github.com/sanneiphyo>
2024-09-20 16:11:33 +0630

    Feat : AppDbContext

---
+++ SEPDotNetCore.ConsoleApp/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SEPDotNetCore.ConsoleApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEPDotNetCore.ConsoleApp
+{
+    internal class AppDbContext : DbContext
+    {
+        protected override void OnConfiguring (DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                string connectionString = "Data Source =.;Initial Catalog=SEPDotNetCore;User ID =sa;Password=sasa@123; TrustServerCertificate=True ";
+                optionsBuilder.UseSqlServer();
+            }
+        }
+
+        public DbSet<BlogDataModel> Blogs { get; set; }
+    }
+}
