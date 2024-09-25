b40a4f614d92a5dd7d170877b77f7f888cd91f94
sanneiphyo <https://github.com/sanneiphyo>
2024-09-25 11:53:32 +0630

    Feat : Console.App2

---
+++ SEPDotNetCore.ConsoleApp2/Program.cs
@@ -0,0 +1,8 @@
+﻿
+// See https://aka.ms/new-console-template for more information
+using SEPDotNetCore.Database.Models;
+
+Console.WriteLine("Hello, World!");
+
+AppDbContext db = new AppDbContext();
+var lst = db.TblBlogs.ToList();
