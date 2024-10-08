6681f8118c47f2dacb1aeb70c1d7a803f2a6a9da
sanneiphyo <https://github.com/sanneiphyo>
2024-10-08 13:10:11 +0630

    feat : Blogdatamodel

---
+++ SEPDotNetCore.RestApi/DataModels/BlogDataModel.cs
@@ -0,0 +1,13 @@
+﻿namespace SEPDotNetCore.RestApi.DataModels
+{
+    public class BlogDataModel
+    {
+        public int BlogId { get; set; }
+        public string BlogTitle { get; set; }
+        public string BlogAuthor { get; set; }
+        public string BlogContent { get; set; }
+        public bool DeleteFlag { get; set; }
+
+
+    }
+}
