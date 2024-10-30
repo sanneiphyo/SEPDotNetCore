dfad80d1195fa2339ab34f2ce796c90e0df31c9b
sanneiphyo <https://github.com/sanneiphyo>
2024-10-30 18:54:44 +0630

    Feat : Add AdoDotNet and Dapper Service

--- SEPDotNetCore.Shared/AdoDotNetService.cs
+++ SEPDotNetCore.Shared/AdoDotNetService.cs
@@ -5,23 +5,26 @@ using System.Data.SqlTypes;
 namespace SEPDotNetCore.Shared
 {
     public class AdoDotNetService
+
     {
         private readonly string _connectionString;
-      
+
         public AdoDotNetService(string connectionString)
         {
             _connectionString = connectionString;
         }
-        public DataTable Query(string query, SqlParameterModel[] sqlParameters)
+        public DataTable Query(string query, params SqlParameterModel[] sqlParameters)
         {
             SqlConnection connection = new SqlConnection(_connectionString);
             connection.Open();
             SqlCommand cmd = new SqlCommand(query, connection);
-            foreach(var sqlParameter in sqlParameters)
+            if (sqlParameters is not null)
             {
-                cmd.Parameters.AddWithValue(sqlParameter.Name, sqlParameter.Value);
+                foreach (var sqlParameter in sqlParameters)
+                {
+                    cmd.Parameters.AddWithValue(sqlParameter.Name, sqlParameter.Value);
+                }
             }
-
             SqlDataAdapter adapter = new SqlDataAdapter(cmd);
             DataTable dt = new DataTable();
             adapter.Fill(dt);
@@ -29,12 +32,38 @@ namespace SEPDotNetCore.Shared
 
             return dt;
         }
-        
+
+        public int Execute(string query, params SqlParameterModel[] sqlParameters)
+        {
+            SqlConnection connection = new SqlConnection(_connectionString);
+            connection.Open();
+            SqlCommand cmd = new SqlCommand(query, connection);
+            if (sqlParameters is not null)
+            {
+                foreach (var sqlParameter in sqlParameters)
+                {
+                    cmd.Parameters.AddWithValue(sqlParameter.Name, sqlParameter.Value);
+                }
+            }
+
+            var result = cmd.ExecuteNonQuery();
+            connection.Close();
+            return result;
+        }
+
+
     }
 
     public class SqlParameterModel
     {
         public string Name { get; set; }
         public object Value { get; set; }
+
+        public SqlParameterModel() { }
+        public SqlParameterModel(string name, object value)
+        {
+            Name = name;
+            Value = value;
+        }
     };
 }
