48ffd8e2441d2a6e4cc1e9f95e82846315b5ccc6
sanneiphyo <sanneiphyo@gmail.com>
2024-11-05 17:37:24 +0630

    feat : BirdMinimalApi

--- SEPDotNetCore.BirdMinimalApi/Program.cs
+++ SEPDotNetCore.BirdMinimalApi/Program.cs
@@ -1,3 +1,5 @@
+using Newtonsoft.Json;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -16,29 +18,45 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
 
-app.MapGet("/weatherforecast", () =>
+app.MapGet("/birds", () =>
 {
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
+    string folderPath = "Data/Birds.json";
+    var jsonStr = File.ReadAllText(folderPath);
+    var result = JsonConvert.DeserializeObject<BirdResponseModel>(jsonStr)!;
+    return Results.Ok(result.Tbl_Bird);
 })
-.WithName("GetWeatherForecast")
+.WithName("GetBirds")
+.WithOpenApi();
+
+
+app.MapGet("/birds{id}", (int id) =>
+{
+    string folderPath = "Data/Birds.json";
+    var jsonStr = File.ReadAllText(folderPath);
+    var result = JsonConvert.DeserializeObject<BirdResponseModel>(jsonStr)!;
+
+    var item = result.Tbl_Bird.FirstOrDefault(x => x.Id == id);
+
+    if (item is null) return Results.BadRequest("No data found");
+    return Results.Ok(item);
+})
+.WithName("GetBird")
 .WithOpenApi();
 
 app.Run();
 
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+
+public class BirdResponseModel
 {
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    public BirdModel[] Tbl_Bird { get; set; }
+}
+
+public class BirdModel
+{
+    public int Id { get; set; }
+    public string BirdMyanmarName { get; set; }
+    public string BirdEnglishName { get; set; }
+    public string Description { get; set; }
+    public string ImagePath { get; set; }
 }
