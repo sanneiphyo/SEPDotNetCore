2e5817b01a9b4a1eb2df8627c0c85412eb3e9716
sanneiphyo <sanneiphyo@gmail.com>
2024-11-11 17:32:46 +0630

    Feat : KpayRestApi

---
+++ SEPDotNetCore.KpayRestApi/Controllers/TransferController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SEPDotNetCore.PayRestApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TransferController : ControllerBase
+    {
+    }
+}
---
+++ SEPDotNetCore.KpayRestApi/Model/PayDataModel.cs
@@ -0,0 +1,34 @@
+﻿namespace SEPDotNetCore.PayRestApi.Model
+{
+    public class UserDataModel
+    {
+        public int UserId { get; set; }
+        public string FullName { get; set; }
+        public string MobileNumber { get; set; }
+        public string Balance { get; set; }
+        public string Pin { get; set; }
+    }
+
+    public class DepositDataModel
+    {
+        public int DepositedId { get; set; }
+        public string MobileNumber { get; set; }
+        public string Balance { get; set; }
+    }
+
+    public class WithDrawDataModel
+    {
+        public int WithDrawId { get; set; }
+        public string MobileNumber { get; set; }
+        public string Balance { get; set; }
+    }
+
+    public class TransferDataModel
+    {
+        public int TranferedId { get; set; }
+        public string FromMobileNumber { get; set; }
+        public string ToMobileNumber { get; set; }
+        public string Amount { get; set; }
+        public string Pin { get; set; }
+    }
+}
