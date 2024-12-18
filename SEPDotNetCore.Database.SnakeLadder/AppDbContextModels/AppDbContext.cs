3f36f86a7c4089e2891ef7e95f138239f8cc6766
sanneiphyo <sanneiphyo@gmail.com>
2024-12-18 20:18:30 +0630

    Feat : Add DI in SnakeGame

--- SEPDotNetCore.Database.SnakeLadder/AppDbContextModels/AppDbContext.cs
+++ SEPDotNetCore.Database.SnakeLadder/AppDbContextModels/AppDbContext.cs
@@ -6,9 +6,7 @@ namespace SEPDotNetCore.SnakeLadder.Database.AppDbContextModels;
 
 public partial class AppDbContext : DbContext
 {
-    public AppDbContext()
-    {
-    }
+  
 
     public AppDbContext(DbContextOptions<AppDbContext> options)
         : base(options)
@@ -23,9 +21,9 @@ public partial class AppDbContext : DbContext
 
     public virtual DbSet<TblPlayer> TblPlayers { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=.;Database=SankeLadderGame;User Id=sa;Password=sasa@123;TrustServerCertificate=True;");
+//    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+//        => optionsBuilder.UseSqlServer("Server=.;Database=SankeLadderGame;User Id=sa;Password=sasa@123;TrustServerCertificate=True;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
--- SEPDotNetCore.SnakeLadder.RestApi/Program.cs
+++ SEPDotNetCore.SnakeLadder.RestApi/Program.cs
@@ -1,3 +1,9 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using SEPDotNetCore.SankeLadder.Domain.Features;
+using SEPDotNetCore.SnakeLadder.Database.AppDbContextModels;
+using SEPDotNetCore.SnakeLadder.RestApi.Controllers;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +12,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(opt =>
+{
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection"));
+},ServiceLifetime.Transient,ServiceLifetime.Transient);
+
+builder.Services.AddScoped<SnakeLadderGameService>();
 
 var app = builder.Build();
 
