a9cc89e88e942273b0c40a7a9512c5747ad732f3
sanneiphyo <sanneiphyo@gmail.com>
2025-01-03 18:09:44 +0630

    feat : MVC CRUD

--- SEPdotNetCore.MvcApp/Controllers/BlogController.cs
+++ SEPdotNetCore.MvcApp/Controllers/BlogController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SEPdotNetCore.MvcApp.Models;
+using SEPDotNetCore.Database.Models;
 using SEPDotNetCore.Domain.Features.Blog;
 
 namespace SEPdotNetCore.MvcApp.Controllers
@@ -23,5 +25,91 @@ namespace SEPdotNetCore.MvcApp.Controllers
         {
             return View("BlogCreate");      
         }
+
+       
+
+        [HttpPost]
+        [ActionName("Save")]
+        public IActionResult BlogSave(BlogRequestModel requestModel)
+        {
+            try
+            {
+                _blogService.CreateBlog(new TblBlog
+                {
+                    BlogAuthor = requestModel.Author,
+                    BlogContent = requestModel.Content,
+                    BlogTitle = requestModel.Title
+                });
+
+                //ViewBag.IsSuccess = true;
+                //ViewBag.Message = "Blog Created Successfully";
+
+                TempData["IsSuccess"] = true;
+                TempData["Message"] = "Blog Created Successfully";
+            }
+            catch (Exception ex)
+            {
+                //ViewBag.IsSuccess = false;
+                //ViewBag.Message = ex.ToString();
+
+                TempData["IsSuccess"] = false;
+                TempData["Message"] = ex.ToString();
+            }
+
+            return RedirectToAction("Index");
+
+            //var lst = _blogService.GetBlogs();
+            //return View("Index", lst);
+        }
+
+        [ActionName("Delete")]
+        public IActionResult BlogDelete(int id)
+        {
+           _blogService.DeleteBlog(id);
+            return RedirectToAction("Index");
+        }
+
+        [ActionName("Edit")]
+        public IActionResult BlogEdit(int id)
+        {
+            var blog = _blogService.GetBlog(id);
+
+            BlogRequestModel blogRequestModel = new BlogRequestModel
+            {
+                Id = blog.BlogId,
+                Author = blog.BlogAuthor,
+                Content = blog.BlogContent,
+                Title = blog.BlogTitle
+            };
+
+            return View("BlogEdit", blog);
+        }
+
+        [HttpPost]
+        [ActionName("Update")]
+        public IActionResult BlogUpdate(int id ,BlogRequestModel requestModel)
+        {
+            try
+            {
+                _blogService.UpdateBlog( id,new TblBlog
+                {
+                    BlogAuthor = requestModel.Author,
+                    BlogContent = requestModel.Content,
+                    BlogTitle = requestModel.Title
+                });
+
+                TempData["IsSuccess"] = true;
+                TempData["Message"] = "Blog Updated Successfully";
+            }
+            catch (Exception ex)
+            {
+              
+                TempData["IsSuccess"] = false;
+                TempData["Message"] = ex.ToString();
+            }
+
+            return RedirectToAction("Index");
+         }
+
     }
 }
---
+++ SEPdotNetCore.MvcApp/Models/BlogRequestModel.cs
@@ -0,0 +1,11 @@
+﻿namespace SEPdotNetCore.MvcApp.Models
+{
+    public class BlogRequestModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Content { get; set; }
+
+    }
+}
