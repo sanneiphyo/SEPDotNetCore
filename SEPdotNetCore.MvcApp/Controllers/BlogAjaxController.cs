940716c3813cb546c7341dd4cad1101560d07dec
sanneiphyo <sanneiphyo@gmail.com>
2025-01-14 13:18:40 +0630

    feat : ajax

---
+++ SEPdotNetCore.MvcApp/Controllers/BlogAjaxController.cs
@@ -0,0 +1,124 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SEPdotNetCore.MvcApp.Models;
+using SEPDotNetCore.Database.Models;
+using SEPDotNetCore.Domain.Features.Blog;
+
+namespace SEPdotNetCore.MvcApp.Controllers
+{
+    public class BlogAjaxController : Controller
+    {
+        private readonly IBlogService _blogService;
+
+        public BlogAjaxController(IBlogService blogService)
+        {
+            _blogService = blogService;
+        }
+
+        [ActionName ("Index")]
+        public IActionResult BlogList()
+        {
+            var lst = _blogService.GetBlogs();
+            return View("BlogList",lst);
+        }
+
+        
+        [ActionName("List")]
+        public IActionResult BlogListAjax()
+        {
+            var lst = _blogService.GetBlogs();
+            return Json(lst);
+        }
+
+        [ActionName("Create")]
+        public IActionResult BlogCreate()
+        {
+            return View("BlogCreate");      
+        }
+
+       
+
+        [HttpPost]
+        [ActionName("Save")]
+        public IActionResult BlogSave(BlogRequestModel requestModel)
+        {
+            try
+            {
+                _blogService.CreateBlog(new TblBlog
+                {
+                    BlogAuthor = requestModel.Author,
+                    BlogContent = requestModel.Content,
+                    BlogTitle = requestModel.Title
+                });
+
+                //ViewBag.IsSuccess = true;
+                //ViewBag.Message = "Blog Created Successfully";
+
+                TempData["IsSuccess"] = true;
+                TempData["Message"] = "Blog Created Successfully";
+            }
+            catch (Exception ex)
+            {
+                //ViewBag.IsSuccess = false;
+                //ViewBag.Message = ex.ToString();
+
+                TempData["IsSuccess"] = false;
+                TempData["Message"] = ex.ToString();
+            }
+
+            return RedirectToAction("Index");
+
+            //var lst = _blogService.GetBlogs();
+            //return View("Index", lst);
+        }
+
+        [ActionName("Delete")]
+        public IActionResult BlogDelete(int id)
+        {
+           _blogService.DeleteBlog(id);
+            return RedirectToAction("Index");
+        }
+
+        [ActionName("Edit")]
+        public IActionResult BlogEdit(int id)
+        {
+            var blog = _blogService.GetBlog(id);
+
+            BlogRequestModel blogRequestModel = new BlogRequestModel
+            {
+                Id = blog.BlogId,
+                Author = blog.BlogAuthor,
+                Content = blog.BlogContent,
+                Title = blog.BlogTitle
+            };
+
+            return View("BlogEdit", blog);
+        }
+
+        [HttpPost]
+        [ActionName("Update")]
+        public IActionResult BlogUpdate(int id ,BlogRequestModel requestModel)
+        {
+            try
+            {
+                _blogService.UpdateBlog( id,new TblBlog
+                {
+                    BlogAuthor = requestModel.Author,
+                    BlogContent = requestModel.Content,
+                    BlogTitle = requestModel.Title
+                });
+
+                TempData["IsSuccess"] = true;
+                TempData["Message"] = "Blog Updated Successfully";
+            }
+            catch (Exception ex)
+            {
+              
+                TempData["IsSuccess"] = false;
+                TempData["Message"] = ex.ToString();
+            }
+
+            return RedirectToAction("Index");
+         }
+
+    }
+}
